bb32fe4b06f7c109be7fcccc8b3f4cbf5e15feb2
Vincent de Groot <vincent@localhost.nl>
2017-01-14 13:44:30 +0100

    Rate me - Button
    Added icon button to settings, which links to Google Play store.

--- src/net/videgro/ships/activities/SettingsActivity.java
+++ src/net/videgro/ships/activities/SettingsActivity.java
@@ -1,12 +1,18 @@
 package net.videgro.ships.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import net.videgro.ships.Analytics;
 import net.videgro.ships.R;
 import net.videgro.ships.fragments.SettingsFragment;
 
 public class SettingsActivity extends Activity {
+	final String TAG="SettingsActivity";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,4 +21,9 @@ public class SettingsActivity extends Activity {
         getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
     }
+    
+	public void onRequestRateClicked(View view){
+		Analytics.logEvent(view.getContext(), TAG, "onRequestRateClicked", "gotoGooglePlay");
+		view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(view.getContext().getString(R.string.app_market_url))))	;
+	}
 }
---
+++ src/net/videgro/ships/preferences/PromoPreferenceRateMe.java
@@ -0,0 +1,26 @@
+package net.videgro.ships.preferences;
+
+import net.videgro.ships.R;
+import android.content.Context;
+import android.preference.Preference;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PromoPreferenceRateMe extends Preference {
+
+	public PromoPreferenceRateMe(Context context) {
+		super(context, null);
+	}
+
+	public PromoPreferenceRateMe(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+	protected View onCreateView(ViewGroup parent) {
+		final LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		return inflater.inflate(R.layout.promo_rate_me, null);
+	}
+}
